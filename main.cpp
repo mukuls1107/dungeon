932edae46db0ee41aac6a7d9eae5e643ae3375f9
mukul1107 <mukulsingh3078@gmail.com>
2024-02-11 18:12:09 +0530

    Attackers Class ✅'

--- main.cpp
+++ main.cpp
@@ -5,8 +5,13 @@ using namespace std;
 
 int main()
 {
-    Dungeon Player1;
-    Player1.displayName();
-    Player1.selectCharacter();
+//     Dungeon Player1;
+//     Attackers hamla;
+//     Player1.displayName();
+//     Player1.selectCharacter();
+//     hamla.Attack(Player1);
+
+    Game Player1;
+    Player1.startGameFromScratch();
     return 0;
 }
--- main/Dungeon.h
+++ main/Dungeon.h
@@ -6,15 +6,18 @@
 #include <windows.h>
 using namespace std;
 
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
 class Dungeon
 {
 
 public:
-    string a;
+    // string a;
     int health = 100;
     int life = 3;
-    const string weapons[3] = {"Twin Shadow Blades", "Valiant Defender", "Staff of Arcane"};
 
+    const string weapons[3] = {"Twin Shadow Blades", "Valiant Defender", "Staff of Arcane"};
+    string characterSelected, weaponGiven;
     void displayName(void)
     {
         cout << "Please Wait.";
@@ -57,13 +60,16 @@ public:
         {
         case 1:
             weaponAlloted = weapons[0];
+            weaponGiven = weapons[0];
             break;
 
         case 2:
             weaponAlloted = weapons[1];
+            weaponGiven = weapons[1];
             break;
         case 3:
             weaponAlloted = weapons[2];
+            weaponGiven = weapons[2];
             break;
         default:
             cout << "Unknown Error Occured!" << endl
@@ -75,8 +81,8 @@ public:
         switch (choice)
         {
         case 1:
-            a = "Adlein";
-            cout << "::: You have selected " << a << " as your character.";
+            characterSelected = "Adlein";
+            cout << "::: You have selected " << characterSelected << " as your character.";
             Sleep(2000);
             cout << "\n:: Keep an eye on your character's health bar which is " << health << "\% .";
             Sleep(2000);
@@ -86,8 +92,8 @@ public:
             break;
 
         case 2:
-            a = "Brian";
-            cout << "\n:: You have selected " << a << " as your character.";
+            characterSelected = "Brian";
+            cout << "\n:: You have selected " << characterSelected << " as your character.";
             Sleep(2000);
             cout << "\n:: Keep an eye on your character's health bar which is " << health << "\% .";
             Sleep(2000);
@@ -96,8 +102,8 @@ public:
             break;
 
         case 3:
-            a = "Cheoron";
-            cout << "\n::: You have selected " << a << " as your character.";
+            characterSelected = "Cheoron";
+            cout << "\n::: You have selected " << characterSelected << " as your character.";
             Sleep(2000);
             cout << "\n::: Keep an eye on your character's health bar which is " << health << "\% .";
             Sleep(2000);
@@ -112,10 +118,85 @@ public:
     }
 };
 
-class GameMechanics
+class Attackers
 {
 public:
+    void Attack(Dungeon &character)
+    {
+        string super[3] = {"Adlein", "Brian", "Cheoron"};
+        int damage[3] = {30, 20, 10};
+        int damageToPerform;
+        string power[3] = {"Twin Shadow Blades", "Valiant Defender", "Staff of Arcane"};
+        if (character.characterSelected == "Adlein")
+        {
+            damageToPerform = damage[0];
+        }
+        else if (character.characterSelected == "Brian")
+        {
+            damageToPerform = damage[1];
+        }
+        else if (character.characterSelected == "Cheoron")
+        {
+            damageToPerform = damage[2];
+        }
+        else
+        {
+            cout << "Out Of Range...";
+            exit(1);
+        }
 
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+
+        cout << character.characterSelected << " is performing an " << character.weaponGiven << "  Attack!" << endl;
+        Sleep(2000);
+        character.health -= damageToPerform;
+
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_GREEN);
+        if (character.health < 0)
+        {
+            cout << "Remaining Health ::> 0" << endl;
+        }
+        else
+        {
+            cout << "Remaining Health ::> " << character.health << endl;
+        }
+    }
 };
 
+class Game
+{
+public:
+    Dungeon Player1;
+    Attackers Attack;
+    void startGameFromScratch()
+    {
+        system("cls");
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset to default color
+
+        Player1.displayName();
+        Player1.selectCharacter();
+        // while (Player1.health > 0)
+        // {
+        //     Attackers attacker;
+        //     attacker.Attack(Player1);
+
+        //     // Check if player's health is 0 or less
+        //     if (Player1.health <= 0)
+        //     {
+        //         cout << "Game Over! Your character has been defeated." << endl;
+        //         break;
+        //     }
+
+        //     char choice;
+        //     cout << "Do you want to continue attacking? (y/n): ";
+        //     cin >> choice;
+        //     if (choice != 'y' && choice != 'Y')
+        //     {
+        //         cout << "You chose to stop attacking." << endl;
+        //         break;
+        //     }
+        // }
+    }
+};
 #endif
